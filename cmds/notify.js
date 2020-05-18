e69123f0cd894c8d6853b71710ab6542cde515cd
Michael Abou Khalil <aboukhalil.michael@gmail.com>
2020-05-18 18:17:50 +0300

    fix bug

--- cmds/notify.js
+++ cmds/notify.js
@@ -3,21 +3,21 @@ module.exports.run = async (bot, message, args, db) => {
     // role check
     const accessRoles = ['Scrim Manager'];
     let canAccess = false;
-    if (message.member.roles.cache.some(r=>accessRoles.includes(r.name))){
+    if (message.member.roles.cache.some(r => accessRoles.includes(r.name))) {
         canAccess = true;
     }
-    if(!canAccess){
+    if (!canAccess) {
         message.reply("you can't use this command!");
         return;
     }
 
     // id check
-    if(typeof focusedID === 'undefined'){
+    if (typeof focusedID === 'undefined') {
         message.channel.send('No scrims planned currently. Please ask your scrim manager to create/select a scrim.');
         return;
     }
 
-    if(args.length != 1){
+    if (args.length != 1) {
         message.channel.send('Missing arguments!');
         return;
     }
@@ -25,52 +25,61 @@ module.exports.run = async (bot, message, args, db) => {
     let playerMessage;
     let subMessage;
 
-    if(args[0] === '1'){
+    if (args[0] === '1') {
         playerMessage = '\nDON\'T FORGET YOU\'RE SCRIMMING TODAY';
         subMessage = '\nDON\'T FORGET YOU\'RE ON THE SUB LIST';
-    }else if(args[0] === '2'){
+    } else if (args[0] === '2') {
         playerMessage = '';
         subMessage = '\nSCRIM IS SOON. BE READY';
-    }else if(args[0] === '3'){
+    } else if (args[0] === '3') {
         playerMessage = '\nGET ONLINE';
         subMessage = '\nSUBS STAY ON STANDBY';
-    }else {
+    } else {
         message.channel.send('Wrong arguments!');
         return;
     }
 
     db.collection('scrims')
-    .orderBy('TimeStamp', 'desc').limit(1)
-    .get()
-    .then(snapshot => {
-        snapshot.forEach(q => {
-            let pIds = q.data().PlayersID;
-            let sIds = q.data().SubsID;
+        .orderBy('TimeStamp', 'desc').limit(1)
+        .get()
+        .then(snapshot => {
+            snapshot.forEach(q => {
+                let emptyPlayers = q.data().PlayersID.length == 0;
+                let emptySubs = q.data().SubsID.length == 0;
+                let pIds = q.data().PlayersID;
+                let sIds = q.data().SubsID;
 
-            let pMsg = '';
-            let sMsg = '';
+                let pMsg = '';
+                let sMsg = '';
 
-            pIds.forEach(p => {
-                pMsg += '<@' + p + '> '
-            });
-            if(pIds != 0){
-                pMsg += playerMessage;
-                message.channel.send(pMsg);
-            }
+                pIds.forEach(p => {
+                    pMsg += '<@' + p + '> '
+                });
+                sIds.forEach(s => {
+                    sMsg += '<@' + s + '> '
+                });
 
-            sIds.forEach(s => {
-                sMsg += '<@' + s + '> '
-            });
-            if(sIds != 0){
-                sMsg += subMessage;
-                message.channel.send(sMsg);
-            }
-            if(sIds == 0 && args[0] === '2'){
-                message.channel.send(sMsg);
-            }
-            
-        })
-    });
+                let reply = '';
+
+                if (!emptyPlayers) {
+                    reply += pMsg + playerMessage;
+                }
+                if (!emptySubs) {
+                    reply += sMsg + subMessage;
+                }
+
+                if (emptySubs && args[0] === '2') {
+                    reply += subMessage;
+                }
+
+                if (emptyPlayers && emptySubs) {
+                    reply += 'No players available yet!';
+                }
+
+                message.channel.send(reply);
+
+            })
+        });
 }
 
 module.exports.help = {
